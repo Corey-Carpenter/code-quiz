d68c3851a4196cc4b17c245553e30919cc12edf8
Corey Carpenter <stephcorey7@gmail.com>
2023-04-13 17:39:56 -0700

    Started the basics of html css and js

---
+++ assets/js/style.js
@@ -0,0 +1,72 @@
+const questions [
+    {
+       question: "question 1",
+       answers: {
+        1: "answer 1",
+        2: "answer 2",
+        3: "answer 3",
+        4: "answer 4",
+       },
+       correctAnswer: "1"
+    },
+
+    {
+        question: "question 1",
+        answers: {
+         1: "answer 1",
+         2: "answer 2",
+         3: "answer 3",
+         4: "answer 4",
+        },
+        correctAnswer: "1"
+     },
+
+     {
+        question: "question 1",
+        answers: {
+         1: "answer 1",
+         2: "answer 2",
+         3: "answer 3",
+         4: "answer 4",
+        },
+        correctAnswer: "1"
+     },
+
+     {
+        question: "question 1",
+        answers: {
+         1: "answer 1",
+         2: "answer 2",
+         3: "answer 3",
+         4: "answer 4",
+        },
+        correctAnswer: "1"
+     },
+
+     {
+        question: "question 1",
+        answers: {
+         1: "answer 1",
+         2: "answer 2",
+         3: "answer 3",
+         4: "answer 4",
+        },
+        correctAnswer: "1"
+     },
+
+     {
+        question: "question 1",
+        answers: {
+         1: "answer 1",
+         2: "answer 2",
+         3: "answer 3",
+         4: "answer 4",
+        },
+        correctAnswer: "1"
+     },
+]
+
+var win = document.querySelector(".win")
+var lose = document.querySelector(".lose")
+var startButton = document.querySelector(".start-button")
+var timerElement = document.querySelector(".timer-count")
