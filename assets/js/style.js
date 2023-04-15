86e8610c2404daecb1a1ff547f68912213fcbf71
Corey Carpenter <stephcorey7@gmail.com>
2023-04-14 21:56:58 -0700

    Added more js for the start button

--- assets/js/style.js
+++ assets/js/style.js
@@ -68,5 +68,29 @@ const questions [
 
 var win = document.querySelector(".win")
 var lose = document.querySelector(".lose")
-var startButton = document.querySelector(".start-button")
+var startButton = document.querySelector("#start-button")
 var timerElement = document.querySelector(".timer-count")
+var WordBlank = document.querySelector(".word-blanks")
+
+var winCounter = 0;
+var loseCounter = 0;
+var numBlanks = 0;
+var timer;
+var timerCount; 
+
+//calls start game functionS
+function startGame() {
+   document.getElementById("#start-button").click();
+   timerCount = 100;
+   startTimer;
+}
+
+function startTimer() {
+
+}
+
+//hides main page and shows quiz
+startButton.addEventListener("click", function () {
+   document.querySelector("#main").setAttribute("class", "show");
+   document.querySelector("#top-page").setAttribute("class", "hide");
+});
